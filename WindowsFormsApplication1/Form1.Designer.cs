7e00eb01716b54dad80abd8c00e11ab6ef1d521c
Tosha <toxatr@icloud.com>
2019-02-16 15:10:06 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(99, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(17, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(79, 31);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Цвет    ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(526, 8);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Число клеток";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(102, 31);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 200);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(551, 34);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(60, 26);
+            this.numericUpDown1.TabIndex = 8;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(551, 76);
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(60, 26);
+            this.numericUpDown2.TabIndex = 10;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(72, 39);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(15, 14);
+            this.pictureBox2.TabIndex = 11;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(508, 36);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 18);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "по X:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(508, 79);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 18);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "по Y:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(17, 78);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Заливка";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(17, 108);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "Краска";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(102, 238);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 18);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "label5";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(638, 271);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Работа с PictureBox";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        Color colorCurrentBox = Color.Gray;
+        private int stepWidth = 0;
+        private int stepHeight = 0;
+        enum mode
+        {
+            fill,
+            click
+        };
+        private mode currentMode = mode.click;
+        
+        public int cellCount
+        {
+            get
+            {
+                int x = Convert.ToInt32(numericUpDown1.Value);
+                int y = Convert.ToInt32(numericUpDown2.Value);
+                return x * y;
+            }
+        }
+        
+        public Form1()
+        {
+            InitializeComponent();
+            updateCellLabel();
+            updateModeLabel();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+         
+           colorDialog1.AllowFullOpen = false;
+           colorDialog1.ShowHelp = true;
+          // colorDialog1.Color = colorCurrentBox;
+           if (colorDialog1.ShowDialog() == DialogResult.OK)
+               colorCurrentBox = colorDialog1.Color;
+           pictureBox2.BackColor = colorCurrentBox;
+// }
+        }
+
+
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            int x = Convert.ToInt32(e.X); // координата по оси X
+            int y = Convert.ToInt32(e.Y); // координата по оси Y
+            label1.Text = x.ToString() + ',' + y.ToString();
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {    
+            Graphics g = Graphics.FromHwnd(pictureBox1.Handle);
+            g.Clear(pictureBox1.BackColor);
+            drawVertical(g);
+            drawHorizontal(g);
+
+            updateCellLabel();
+        }
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            Graphics g = Graphics.FromHwnd(pictureBox1.Handle);
+            g.Clear(pictureBox1.BackColor);
+            drawHorizontal(g);
+            drawVertical(g);
+
+            updateCellLabel();
+        }
+
+        private void drawHorizontal(Graphics g)
+        {
+            int boxWidth = pictureBox1.Width;
+            int boxHeight = pictureBox1.Height;
+            int boxStepHeight = Convert.ToInt32(numericUpDown2.Value);
+            stepHeight = boxHeight / boxStepHeight;
+
+            Pen myPen = new Pen(Color.Gray, 1);
+            for (int i = 1; i < boxStepHeight; i++)
+            {
+                g.DrawLine(myPen, 0, i * stepHeight, boxWidth, i * stepHeight);
+            }
+
+        }
+        private void drawVertical(Graphics g)
+        {
+            int boxWidth = pictureBox1.Width;
+            int boxHeight = pictureBox1.Height;
+            int boxStepWidth = Convert.ToInt32(numericUpDown1.Value);
+            stepWidth = boxWidth / boxStepWidth;
+
+            Pen myPen = new Pen(Color.Gray, 1);
+            for (int i = 1; i < boxStepWidth; i++)
+            {
+                g.DrawLine(myPen, i * stepWidth, 0, i * stepWidth, boxHeight);
+            }
+        }
+
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+           if(currentMode == mode.click)
+            {
+                fillOne(e);
+            }
+           else
+            {
+                fill(e);
+            }
+        }
+
+        private void fillOne(MouseEventArgs e) 
+        {
+            Graphics g = Graphics.FromHwnd(pictureBox1.Handle);
+            //SolidBrush - сплошная закраска цветом кисти;
+            //TextureBrush - наложение картинки (image) на область закраски;
+            //HatchBrush - закраска области предопределенным узором;
+            //LinearGradientBrush - сплошная закраска c переходом цвета кисти (градиентная закраска);
+            //PathGradientBrush -сплошная закраска c переходом цвета кисти по специальному алгоритму.
+            SolidBrush myBrush = new SolidBrush(colorCurrentBox);
+
+
+            int boxWidth = pictureBox1.Width;
+            int boxHeight = pictureBox1.Height;
+
+            int boxStepWidth = Convert.ToInt32(numericUpDown1.Value);
+            int stepWidth = boxWidth / boxStepWidth;
+
+            int boxStepHeight = Convert.ToInt32(numericUpDown2.Value);
+            int stepHeight = boxHeight / boxStepHeight;
+
+            int x = Convert.ToInt32(e.X); // координата по оси X
+            int y = Convert.ToInt32(e.Y); // координата по оси Y           
+
+
+            x = x / stepWidth;
+            y = y / stepHeight;
+            g.FillRectangle(myBrush, x * stepWidth, y * stepHeight, stepWidth, stepHeight);
+        }
+
+        private void fill(MouseEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            currentMode = mode.fill;
+            updateModeLabel();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            currentMode = mode.click;
+            updateModeLabel();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void updateModeLabel()
+        {
+            label5.Text = "Current mode is " + (currentMode == mode.click ? "fillOne" : "fill");
+        }
+        private void updateCellLabel()
+        {
+            label2.Text = "Cell count = " + cellCount;
+        }
+    }
+}
